2cd7287341020e25ef0d0454afe4c855bb5e7221
Ederam <53803895+Ederam@users.noreply.github.com>
2025-03-05 18:14:12 -0500

    Ajuste Navbar
    Se configura navbar parq eu funcione en la pagina principal

--- AppGestionEstudiantes/src/app/Pages/navbar/navbar.component.ts
+++ AppGestionEstudiantes/src/app/Pages/navbar/navbar.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatToolbar } from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [MatToolbar,RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
