d5db3629b420ca16a860ab777c48eb1913e8d854
Ederam <53803895+Ederam@users.noreply.github.com>
2025-03-06 17:41:18 -0500

    ajuste menus
    Se ajusta para visualizacion de los menus con estilo, los botones del menu

--- AppGestionEstudiantes/src/app/Pages/navbar/navbar.component.ts
+++ AppGestionEstudiantes/src/app/Pages/navbar/navbar.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 import { MatToolbar } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatToolbar,RouterLink],
+  imports: [MatToolbar,RouterLink,MatButtonModule],
+  // imports: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
